9c5de13c4b83ffb794a87a41b8a8bfef90cebb7f
YMD7 <hysteric.madam@gmail.com>
2012-12-25 00:23:13 +0900

    Gallery Album Page Update

---
+++ gallery/vietnam.php
@@ -0,0 +1,100 @@
+<?php include "../header_album.php" ?>
+
+<!-- MAIN -->
+  <div id="main">
+    
+    <!-- GLOBAL NAVI -->
+    <?php include "../gnavi.php" ?>
+
+    <div id="right_column">
+      <h2 class="large">Gallery</h2>
+      <div class="gallery_content gallery_album">
+      <h3>ベトナム - Vietnam</h3>
+
+      <ul class="tab_selector">
+        <li class="current">2007.01</li>
+        <li>2008.01</li>
+        <li>2009.01</li>
+        <li>2009.01</li>
+        <li>2009.01</li>
+        <li>2009.01</li>
+        <li>2009.01</li>
+      </ul>
+
+      <div id="gallery_area">
+
+	      <div>
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+   	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+	      <div class="hidden">
+	      <ul class="sumbnails">
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_yoko.jpg"><img src="../images/sumb_sample_yoko.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	        <li><a class="fancybox" rel="group" href="../images/sample_tate.jpg"><img src="../images/sumb_sample_tate.jpg" alt="" /></a></li>
+	      </ul>
+	      </div>
+
+      </div>
+
+    </div>
+<!-- End Right-culumn -->
+  </div>
+<!-- End MAIN -->
+
+
+
+</div>
+</body>
+</html>
--- gnavi.php
+++ gnavi.php
@@ -1,9 +1,9 @@
     <div id="gnavi">
       <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="http://localhost/~kyo_YMD/SayakaSuzuki/index.php">Home</a></li>
+        <li><a href="http://localhost/~kyo_YMD/SayakaSuzuki/profile.php">Profile</a></li>
+        <li><a href="http://localhost/~kyo_YMD/SayakaSuzuki/#">Blog</a></li>
+        <li><a href="http://localhost/~kyo_YMD/SayakaSuzuki/gallery.php">Gallery</a></li>
+        <li><a href="http://localhost/~kyo_YMD/SayakaSuzuki/#">Contact</a></li>
       </ul>
     </div>
